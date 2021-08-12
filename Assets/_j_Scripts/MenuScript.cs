ba7a5a7b95a5b755cb2a70fa85806fdd26262271
locojoetive <johannes.mic@gmail.com>
2021-08-12 23:17:16 +0200

    Revert "change color names on win screen"
    This reverts commit a13ca015ed9ce906371ddbabbb9f9ab14620196c.

--- Assets/_j_Scripts/MenuScript.cs
+++ Assets/_j_Scripts/MenuScript.cs
@@ -75,8 +75,8 @@ public class MenuScript : MonoBehaviour
         buttonRestart.SetActive(active);
         textWin.gameObject.SetActive(active);
         string theWinnerIs = player == Player.PLAYER_1
-            ? "PINK"
-            : "PURPLE";
+            ? "RED"
+            : "BLUE";
         theWinnerIs += " WINS";
         textWin.text = theWinnerIs;
         isGameOver = true;
